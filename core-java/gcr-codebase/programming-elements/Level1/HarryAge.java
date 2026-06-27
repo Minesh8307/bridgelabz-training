518a9eb03beda1afa8a6b5f30a85e803eadf9f29
Minesh8307 <minesh.gla_cs23@gla.ac.in>
2026-06-27 17:13:25 +0530

    update folder

--- core-java/gcr-codebase/programming-elements/Level1/HarryAge.java
+++
@@ -1,9 +0,0 @@
-public class HarryAge {
-    public static void main(String[] args) {
-        int harryBirthYear = 2000;
-        int currentYear = 2024;
-        int harryAge = currentYear - harryBirthYear;
-
-        System.out.println("Harry's age in " + currentYear + " is: " + harryAge);
-    }
-}
